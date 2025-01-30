346766e47ec4a2c9b146d4032fa5302b6b51a7cd
Salvador HM <salvadorhm@gmail.com>
2025-01-29 19:56:32 -0600

    UPDATED firebase demo

--- 11_firebase/app.py
+++ 11_firebase/app.py
@@ -3,12 +3,13 @@ import json
 
 URI= "https://dbnube-3b225-default-rtdb.firebaseio.com/personas.json"
 
+# GET data
 response = requests.get(URI)
 
 print(f"RESPONSE: {response}")
 print(f"RESPONSE: {response.text}")
 
-
+# CREATE data
 data = {
     "005":
         {
@@ -19,6 +20,10 @@ data = {
 
 print(f"DATA: {data}")
 response = requests.patch(URI,json.dumps(data))
-
 print(f"RESPONSE: {response}")
 
+
+# DELETE data
+URI= "https://dbnube-3b225-default-rtdb.firebaseio.com/personas/001.json"
+response = requests.delete(URI)
+print(f"RESPONSE: {response}")
